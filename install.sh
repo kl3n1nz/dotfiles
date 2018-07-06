e240ec3d55e1e306916b83b76f551107b725547c
Egor Klenin <yegorklenin@gmail.com>
2018-07-06 11:24:40 +0300

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+sudo apt update && sudo apt upgrade
+
+sudo apt-get install -y vim-gtk
+sudo apt install tmux
+
+sudo snap install htop
+
+sudo apt install vlc
+
+sudo apt install tlp tlp-rdw
+
+sudo snap install tree
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
@@ -13,6 +26,8 @@ sudo apt-get install telegram
 
 # skype
 sudo snap install skype --classic
+
+
 
 
 # chrome
@@ -40,5 +55,55 @@ sudo apt install g++
 
 
 sudo apt-get install python-dev python3-dev
+sudo apt-get install python3-tk python3.6-tk
 sudo apt-get install libevent-dev
 sudo apt-get install build-essential
+
+
+# https://wiki.ubuntu.com/ClipboardPersistence
+sudo apt-get install parcellite 
+
+sudo apt install python-pip
+
+sudo -H pip install virtualenvwrapper
+
+sudo apt install postgresql-client-common
+sudo apt install postgresql-client-10
+pip install -U pgcli
+
+sudo apt install gnome-tweak-tool
+
+sudo apt install ubuntu-restricted-extras
+
+
+# docker
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+# add user to docker group
+sudo groupadd docker
+udo gpasswd -a $USER docker
+newgrp docker
+
+
+# docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
